cbb15062762949a8459b8a3344a123fad2719645
name2077 <78960810+Name2077@users.noreply.github.com>
2021-02-20 17:59:02 +0300

    1.2.2
    com

---
+++ auth.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width">
+    <title>Liberty</title>
+  </head>
+  <body>
+    <center><h3>liberty.reg</h3></center>
+<form class="" action="/Applications/MAMP/htdocs/liberty/see.php" method="post">
+
+<center><input type="text" name="" value="" placeholder="Имя" id="space"></center>
+<center><input type="text" name="" value="" placeholder="Фамилия" id="max"></center>
+<center><input type="password" name="" value="" placeholder="Пароль" id="red"></center>
+<center><button type="submit">Вход</button></center>
+</form>
+<?php
+require 'connect.php';
+ ?>
+<style media="screen">
+  body {
+    font-family: Tahoma;
+    background: lightgrey;
+    margin: 0;
+    background-image: url();
+  }
+  button {
+    margin-top: 10px;
+    cursor: pointer;;
+    padding: 10px 30px;
+    height: 50px;
+
+    color: #fff;
+    background: black;
+    padding-left: 25px;
+    border-radius: 10px/10px;
+    border: none;
+    text-transform: uppercase;
+    font-size: 15px;
+    box-shadow: 2px 2px 5px rgba(0,0,0,0,82);
+  }
+  input {
+    margin-top:  5px;
+    margin-left:  10px;
+    height: 30px;
+    width: 300px;
+      color: #fff;
+    background: black;
+    padding-left: 15px;
+    border-radius: 19px/20px;
+    border: none;
+    font-size: 18px;
+    box-shadow: 2px 2px 5px rgba(0,0,0,0.82);
+    outline: none;
+    border: none;
+    border: solid 1px #f2f2f2;
+    .custom-file-upload {
+        border: 1px solid #@media screen and (max-width: 1200px) {
+        .lll form {
+          margin-top: -120%;
+          width: 1000px;
+          height: 70px;
+        }
+    @media screen and (max-width: 1200px) {
+    .lll form {
+      margin-left: -123%;
+
+    }
+    .lll input {
+    margin-top: 405%;
+    margin-left: 10%;
+    }
+    @media screen and (max-width: 700px;) {
+      .lll form {
+          max-width: none;
+          margin-top: none;
+      }
+    }
+    main {
+      flex-direction: column;
+    }
+</style>
+  </body>
+</html>
